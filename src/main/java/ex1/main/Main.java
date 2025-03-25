5438c095684388191a6dcc9d3dabdf7351812cf4
adriaVH <adriavargashernandez@gmail.com>
2025-03-25 13:45:54 +0100

    set up

---
+++ src/main/java/ex1/main/Main.java
@@ -0,0 +1,30 @@
+package ex1.main;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Per a tots els exercicis s'ha d'utilitzar l'API de Java Lambdas and Streams de Java 8+.
+
+        - Exercici 1
+        A partir d’una llista de Strings, escriu un mètode que retorna una llista de totes les
+        cadenes que contenen la lletra ‘o’. Imprimeix el resultat.
+         - Exercici 2
+        Has de fer el mateix que en el punt anterior, però ara, el mètode ha de retornar una llista
+        amb els Strings que a més de contenir la lletra ‘o’ també tenen més de 5 lletres. Imprimeix el resultat.
+        - Exercici 3
+        Crea una llista amb els noms dels mesos de l’any. Imprimeix tots els elements de la llista
+        amb una lambda.
+        - Exercici 4
+        Fer la mateixa impressió del punt anterior, però mitjançant method reference.
+        - Exercici 5
+        Crea una Functional Interface amb un mètode anomenat getPiValue() que ha de retornar un double.
+            Des del main() de la classe principal, instància la interfície i assigna-li el valor 3.1415.
+                Invoca el mètode getPiValue() i imprimeix el resultat.
+        - Exercici 6
+        Crea una llista amb nombres i cadenes de text i ordena la llista amb les cadenes de més curta a
+        més llarga.
+        */
+
+        System.out.println("Hello world!");
+    }
+}
