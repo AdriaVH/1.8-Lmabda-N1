97111d072ac6281071c57abbb2dfb251605b035f
adriaVH <adriavargashernandez@gmail.com>
2025-03-26 12:54:14 +0100

    ex8 finished

--- src/main/java/ex1/interfaces/Pi.java
+++
@@ -1,5 +0,0 @@
-package ex1.interfaces;
-@FunctionalInterface
-public interface Pi {
-    double getPiValue();
-}
--- src/main/java/ex1/main/Main.java
+++ src/main/java/ex1/main/Main.java
@@ -1,72 +1,31 @@
 package ex1.main;
 
-import ex1.interfaces.Pi;
-
 import java.util.*;
 
 public class Main {
-
-    @FunctionalInterface
-    interface PiValue {
-        double getPiValue();
-    }
-
     public static void main(String[] args) {
-        /*
-        Per a tots els exercicis s'ha d'utilitzar l'API de Java Lambdas and Streams de Java 8+.
-
-        - Exercici 1
-        A partir d’una llista de Strings, escriu un mètode que retorna una llista de totes les
-        cadenes que contenen la lletra ‘o’. Imprimeix el resultat.
-         - Exercici 2
-        Has de fer el mateix que en el punt anterior, però ara, el mètode ha de retornar una llista
-        amb els Strings que a més de contenir la lletra ‘o’ també tenen més de 5 lletres. Imprimeix el resultat.
-        - Exercici 3
-        Crea una llista amb els noms dels mesos de l’any. Imprimeix tots els elements de la llista
-        amb una lambda.
-        - Exercici 4
-        Fer la mateixa impressió del punt anterior, però mitjançant method reference.
-        - Exercici 5
-        Crea una Functional Interface amb un mètode anomenat getPiValue() que ha de retornar un double.
-            Des del main() de la classe principal, instància la interfície i assigna-li el valor 3.1415.
-                Invoca el mètode getPiValue() i imprimeix el resultat.
-        - Exercici 6
-        Crea una llista amb nombres i cadenes de text i ordena la llista amb les cadenes de més curta a
-        més llarga.
-        */
-
-        /*
         String[] words = {
                 "apple", "orange", "table", "ocean", "window",
                 "chair", "robot", "mountain", "door", "shadow",
                 "pencil", "forest", "laptop", "jellyfish", "tornado"
         };
         System.out.println("Exercici 1");
-        List<String> wordsList = Arrays.asList(words);
-        List wordsWithLetterO = wordsList.stream()
-                .filter(s -> s.contains("o")).toList();
-
-        System.out.println(wordsWithLetterO);
+        System.out.println(makeListOfWordsWithLetterO(Arrays.stream(words).toList()));
 
         System.out.println("\nExercici 2");
-        List wordsWithLetterOAndMoreThan5Letters = wordsList.stream()
-                .filter(s -> s.contains("o") && s.length() > 5)
-                .toList();
-        System.out.println(wordsWithLetterOAndMoreThan5Letters);
+        System.out.println(makeListOfWordsWithLetterOLengthMoreThan5(Arrays.stream(words).toList()));
 
         System.out.println("\nExercici 3");
-
         List<String> months = Arrays.asList("January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November", "December");
 
         months.forEach(month -> System.out.print(month + " "));
+        System.out.println("\n");
 
-        System.out.println("\n\nExercici 4");
-
+        System.out.println("Exercici 4");
         months.forEach(System.out::println);
 
         System.out.println("\nExercici 5");
-
         Pi pi1 = new Pi() {
             @Override
             public double getPiValue() {
@@ -77,13 +36,37 @@ public class Main {
 
         System.out.println(pi1.getPiValue());
         System.out.println(pi2.getPiValue());
-*/
-        System.out.println("Exercici 6");
 
-        List<Object> mixedList = Arrays.asList(42, "Lambda", 3.14, "Java", 100, "Hello", 7.5, "Stream");
-
-        mixedList.stream().sorted();
+        System.out.println("\nExercici 6");
+        List<Object> mixedList = Arrays.asList(42, "Lambda", 3.1415, "Java", 100, "HelloLambdas", 7.5, "Stream");
+        mixedList.sort( (x, y) -> Integer.compare(x.toString().length(), y.toString().length()));
         System.out.println(mixedList);
 
+        System.out.println("\nExercici 7");
+        System.out.println(mixedList.reversed());
+
+        System.out.println("\nExercici 8");
+        String string = "ITAcademy";
+        System.out.println(string);
+        ReverseMethod reverseMethod = (s) -> new StringBuilder(s).reverse().toString();
+
+        string = reverseMethod.reverse(string);
+        System.out.println(string);
+
+
+    }
+    public static List<String> makeListOfWordsWithLetterO (List <String> list) {
+        return list.stream().filter(s -> s.contains("o")).toList();
+    }
+    public static List<String> makeListOfWordsWithLetterOLengthMoreThan5 (List <String> list) {
+        return makeListOfWordsWithLetterO(list.stream().filter(x -> x.length() > 5).toList());
+    }
+    @FunctionalInterface
+    public interface Pi {
+        double getPiValue();
+    }
+    @FunctionalInterface
+    public interface  ReverseMethod {
+        String reverse (String s);
     }
 }
